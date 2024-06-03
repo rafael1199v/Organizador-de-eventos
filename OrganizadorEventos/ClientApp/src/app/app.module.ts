216d3d15ee0c56731e665a51bdad8e9592d0c933
Matipala <josue.molina@ucb.edu.bo>
2024-06-03 17:07:35 -0400

    registro
    implemente registro

--- OrganizadorEventos/ClientApp/src/app/app.module.ts
+++ OrganizadorEventos/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -29,6 +31,7 @@ import { LoginComponent } from './login/login.component';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'login', component: LoginComponent},
+      { path: 'register', component: RegisterComponent},
     ])
   ],
   providers: [],
