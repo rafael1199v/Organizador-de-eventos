70677fb6af9c1e47d433f51b92ac778bba53be91
Matipala <josue.molina@ucb.edu.bo>
2024-06-03 16:47:45 -0400

    Login
    xd

--- OrganizadorEventos/ClientApp/src/app/app.component.ts
+++ OrganizadorEventos/ClientApp/src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  showNavbar = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        const url = event.urlAfterRedirects;
+        this.showNavbar = this.shouldShowNavbar(url);
+      });
+  }
+
+  shouldShowNavbar(url: string): boolean {
+    // Define las rutas donde NO quieres mostrar el navbar
+    const noNavbarRoutes = ['/login', '/register'];
+    return !noNavbarRoutes.includes(url);
+  }
 }
