50a901599a1cd8e7f0fc4c0256e04222b6abb2fe
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2024-06-06 15:59:49 -0400

    Actualizado team-event y home
    si

--- OrganizadorEventos/ClientApp/src/app/team-event-list/team-event-list.component.ts
+++ OrganizadorEventos/ClientApp/src/app/team-event-list/team-event-list.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team-event-list',
-  templateUrl: './team-event-list.component.html'
+  templateUrl: './team-event-list.component.html',
+  styleUrls: ['./team-event-list.component.css']
 })
 export class TeamEventListComponent {
   public currentCount = 0;
