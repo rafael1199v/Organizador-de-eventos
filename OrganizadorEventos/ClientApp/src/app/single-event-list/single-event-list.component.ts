89c115799b080c900dd046778edce3be3cf55744
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2024-06-06 11:08:04 -0400

    Arreglos en eventos individuales
    Aquí no acaba la cosa

--- OrganizadorEventos/ClientApp/src/app/single-event-list/single-event-list.component.ts
+++ OrganizadorEventos/ClientApp/src/app/single-event-list/single-event-list.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-single-event-list',
-  templateUrl: './single-event-list.component.html'
+  templateUrl: './single-event-list.component.html',
+  styleUrls: ['./single-event-list.component.css']
 })
 export class SingleEventListComponent {
   public currentCount = 0;
