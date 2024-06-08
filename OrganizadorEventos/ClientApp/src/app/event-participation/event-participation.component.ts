d94ff150aa5105b1ac057e7b54664c56c14ed58e
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2024-06-08 17:39:22 -0400

    Participacion de evento
    falta editar el app.module.ts, pero no quiero de momento o tendré conflictos

---
+++ OrganizadorEventos/ClientApp/src/app/event-participation/event-participation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-event-participation',
+    templateUrl: './event-participation.component.html',
+    styleUrls: ['./event-participation.component.css']
+  })
+  export class EventParticipationComponent {
+
+  }
+  
